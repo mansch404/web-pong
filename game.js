96a37decc4a1fe5ee34bd05a979f955f2d3d0363
mansch404 <mansch404@gmail.com>
2021-08-05 15:18:42 +0200

    Comments added

--- game.js
+++ game.js
@@ -1,18 +1,23 @@
+
+/*
+* PONG GAME
+*/
+
+// Variables
+
 var humanPlayer;
 var computerPlayer;
-var moving;
+var ball
+
+// Document Event Listener
 
 document.addEventListener("DOMContentLoaded", function() { console.log("content loaded");
     humanPlayer = document.getElementById("humanPlayer");
     computerPlayer = document.getElementById("computerPlayer");
+    ball = document.getElementById("ball");
     layout(humanPlayer, 45, 0.5);
     layout(computerPlayer, 45, 98.5);
-    layout(document.getElementById("ball"), 49.5, 49.5, 3, 3)
-});
-
-window.addEventListener( "resize", function() {
-    layout(computerPlayer, 45, 98.5);
-    layout(document.getElementById("ball"), 49.5,49.5,3,3,500);
+    layout(ball, 49.5, 49.5, 3, 3)
 });
 
 document.addEventListener('keydown', function() {
@@ -25,7 +30,14 @@ document.addEventListener('keydown', function() {
     }
 });
 
-document.addEventListener('keyup', function() {moving = false;});
+// Window Event Listener
+
+window.addEventListener( "resize", function() {
+    layout(computerPlayer, 45, 98.5);
+    layout(ball, 49.5,49.5,3,3,500);
+});
+
+// Functions
 
 function move(element, direction, distance=200, duration=1000) {
     var topOrLeft = (direction=="left" || direction=="right") ? "left" : "top";
@@ -36,7 +48,6 @@ function move(element, direction, distance=200, duration=1000) {
     var destination = Number(value) + distance;
     var frameDistance = distance / (duration / 10);
     if(!(Number(elStyle.getPropertyValue("top").replace("px","")) == 0)) {
-        moving = true;
         function moveAFrame() {
         elStyle = window.getComputedStyle(element);
         value = elStyle.getPropertyValue(topOrLeft).replace("px", "");
